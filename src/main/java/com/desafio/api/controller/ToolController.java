daf0551eada8e4d0d901c97515184fcefe01d7b6
MarcosSilva13 <marcosslp45@gmail.com>
2023-12-30 11:40:39 -0300

    Feat: add documentation

--- src/main/java/com/desafio/api/controller/ToolController.java
+++ src/main/java/com/desafio/api/controller/ToolController.java
@@ -3,6 +3,11 @@ package com.desafio.api.controller;
 import com.desafio.api.dto.ToolRequestDTO;
 import com.desafio.api.dto.ToolResponseDTO;
 import com.desafio.api.service.ToolService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tools")
+@Tag(name = "Tool")
 public class ToolController {
 
     private final ToolService toolService;
@@ -21,16 +27,30 @@ public class ToolController {
         this.toolService = toolService;
     }
 
+    @Operation(summary = "Get all tools", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping
     public ResponseEntity<List<ToolResponseDTO>> getAll() {
         return ResponseEntity.ok().body(toolService.getAllTools());
     }
 
+    @Operation(summary = "Get all tools by tag name", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Data not found", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/findByTag")
     public ResponseEntity<List<ToolResponseDTO>> getByTag(@RequestParam(name = "tag") String tag) {
         return ResponseEntity.ok().body(toolService.getAllToolsByTag(tag));
     }
 
+    @Operation(summary = "Save new tool", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping
     public ResponseEntity<ToolResponseDTO> save(@RequestBody @Valid ToolRequestDTO requestDTO) {
         ToolResponseDTO toolResponse = toolService.saveTool(requestDTO);
@@ -44,6 +64,11 @@ public class ToolController {
         return ResponseEntity.created(location).body(toolResponse);
     }
 
+    @Operation(summary = "Delete tool", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "404", description = "Data not found", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         toolService.deleteTool(id);
